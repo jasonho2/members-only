81e5fb70556f24bcb852dae1ce23092fb5180898
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2025-04-30 13:39:40 -0700

    Allow user to type username into sign up form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@rails/ujs"
-Rails.start()
+import "@rails/ujs";
+Rails.start();
 
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
