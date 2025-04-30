259c7213ae41d3059f7c34b8a94e91f66233a778
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2025-04-30 13:31:41 -0700

    Show posts in reverse chronological order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create ]
 
   def index
-    @post = Post.all
+    @post = Post.order(created_at: :desc)
   end
 
   def new
