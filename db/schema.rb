39d120976fd48c25e95052b0d3f32c2faa896978
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2025-04-30 13:51:37 -0700

    Fix username during signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_30_161853) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_30_204356) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
@@ -32,6 +32,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_30_161853) do
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "posts", "users"
