b51f259279d85228bd5f2f9ffebf1022e919f0f7
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2025-04-30 09:42:49 -0700

    Modify schema for user_id, fix index, uncomment username

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,28 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
+
   def index
+    @post = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path, success: "Post was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+      flash.now[:error] = "An error occured!"
+    end
+  end
+
+  private
+
+  def post_params
+    params.expect(post: [ :title, :content ])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_29_181956) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_30_161853) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_29_181956) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
